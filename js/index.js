5ddcfd91abfb14b9c8b49c602badabd51c0b8987
Павел Лукьянов <pahan-01.04.10@mail.ru>
2023-07-18 18:02:57 +0500

    add hero section, footer, header, and mobile menu

--- js/index.js
+++ js/index.js
@@ -1,9 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let burgerMenu = document.getElementById('burger');
-    if (burgerMenu) {
-        burgerMenu.addEventListener('click', () => {
-            burgerMenu.classList.toggle('opened');
+    let burgerMenus = document.querySelectorAll('.burger__toggle');
+    let mobileMenu = document.querySelector('.js-menu-container');
+
+    if (burgerMenus.length > 0) {
+
+        burgerMenus.forEach(el => {
+            el.addEventListener('click', () => {
+                mobileMenu.classList.toggle('opened');
+                burgerMenus.forEach(el => {
+                    el.classList.toggle('opened');
+                })
+            })
+        })
+    }
+
+    if (window.outerWidth <= 1100) {
+        let headerContacts = document.querySelector('.header__info__container');
+        let menuContactsContainr = document.querySelector('.dropdown__contacts');
+        menuContactsContainr.innerHTML = headerContacts.innerHTML;
+        headerContacts.remove();
+
+        let mainMenu = document.querySelector('.js-mobile');
+        let mobileMenuContainer = document.querySelector('.mobile__menu');
+
+        mobileMenuContainer.innerHTML = mainMenu.innerHTML;
+        mainMenu.remove();
+
+
+        let btnsShowMenu = document.querySelectorAll('.menu__arrow');
+
+        btnsShowMenu.forEach(el => {
+            el.addEventListener('click', () => {
+                el.closest('.header__item-dropdown').classList.toggle('active');
+            })
         })
     }
 
